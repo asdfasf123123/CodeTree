b80afade9d63e1e769ca5113bb7b1d39570e319a
asdfasf123123 <128668708+asdfasf123123@users.noreply.github.com>
2024-11-16 17:59:17 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241116/방향에 맞춰 이동/move-in-direction.cpp
+++ 241116/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// 방향 벡터!
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, -1, 0, 1};
+
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int mapper[200];
+    mapper['E'] = 0;
+    mapper['S'] = 1;
+    mapper['W'] = 2;
+    mapper['N'] = 3;
+
     int N;
     cin >> N;
     int x = 0, y = 0;
 
-    // 방향 벡터!
-    int dx[] = {1, 0, -1, 0};
-    int dy[] = {0, -1, 0, 1};
+    for (int i = 0; i < N; i ++) {
+        char dirChar;
+        int size;
+        cin >> dirChar >> size;
+        int dirNum = mapper[dirChar];
+        x = x + dx[dirNum] * size;
+        y = y + dy[dirNum] * size;
+    }
+
+    cout << x << " " << y;
+    return 0;
+}
 
     // if (dirNum == 0) {
     //     nx = x + d[0], ny = y + d[0];
@@ -23,26 +41,19 @@ int main() {
     // else if (dirNum == 3) {
     //     nx = x + d[3], ny = y + d[3];
     // }
-    for (int i = 0; i < N; i++) {
-        // 이동에 대한 코드는 고정
-        char dirChar;
-        int size, dirNum;
-        cin >> dirChar >> size;
-        if (dirChar == 'N') {
-            dirNum = 3;
-        } else if (dirChar == 'E') {
-            dirNum = 0;
-        } else if (dirChar == 'S') {
-            dirNum = 1;
-        } else if (dirChar == 'W') {
-            dirNum = 2;
-        }
 
-        x = x + dx[dirNum] * size;
-        y = y + dy[dirNum] * size;
-    }
 
-    cout << x << " " << y;
-
-    return 0;
-}
+// for (int i = 0; i < N; i++) {
+//         // 이동에 대한 코드는 고정
+//         char dirChar;
+//         int size, dirNum;
+//         cin >> dirChar >> size;
+//         if (dirChar == 'N') {
+//             dirNum = 3;
+//         } else if (dirChar == 'E') {
+//             dirNum = 0;
+//         } else if (dirChar == 'S') {
+//             dirNum = 1;
+//         } else if (dirChar == 'W') {
+//             dirNum = 2;
+//         }
