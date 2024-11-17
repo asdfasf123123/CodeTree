667d8a8f6da0ed81403c8ab63a19fb33ec303403
asdfasf123123 <128668708+asdfasf123123@users.noreply.github.com>
2024-11-18 00:21:02 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 241118/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -12,6 +12,11 @@ int main() {
         }
     }
 
+    if (n == 1 && m == 1) {
+        cout << 2;
+        exit(0);
+    }
+
     for (int i = 0; i < n; i++) {
         int numrow = 1;
         int numcol = 1;
@@ -19,13 +24,20 @@ int main() {
             if (arr[i][j] == arr[i][j - 1])
                 numrow++;
             else numrow = 1;
-
-            if (arr[j][i] == arr[j - 1][i])
-                numcol++;
-            else numcol = 1;
+            if (numrow == m) {
+                cnt++;
+                break;
+            }
         }
-        if (numrow >= m) cnt++;
-        if (numcol >= m) cnt++;
+        for (int j = 1; j < n; j++) {
+            if (arr[j][i] == arr[j - 1][i])
+                    numcol++;
+                else numcol = 1;
+                if (numcol == m) {
+                    cnt++;
+                    break;
+                }
+        }
     }
     cout << cnt;
     return 0;
