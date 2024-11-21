9c0257459528dff96957c6f39b92e581f0f8d0b5
asdfasf123123 <128668708+asdfasf123123@users.noreply.github.com>
2024-11-21 19:40:12 +0900

    Title: 떨어지는 1자 블록, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241121/떨어지는 1자 블록/falling-horizontal-block.cpp
+++ 241121/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -1,42 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int n,m;
 int arr[100][100];
 
-void block_down(int m, int r) {
-    for (int row = 0; row < n; row++) {
-        for (int i = r; i < r + m; i++) {
-            if (arr[row][i] != 0) {
-                stop_block(row, r);
+void stop_block(int now, int col) {
+    if (now == -1) return;
+
+    for (int i = col; i < (col + m); i++) {
+        arr[now][i] = 1;
+    }
+}
+
+void block_down(int r) {
+    for (int now = -1; now < n-1; now++) {
+        int next = now + 1;
+        for (int col = r; col < (r + m); col++) {
+            if (next == n || arr[next][col] != 0) {
+                stop_block(now, col);
                 return;
             }
         }
     }
 }
 
-void stop_block(int row, int r) {
-    for (int i = r; i < r + m; i++) {
-        arr[row][r] = 1;
-    }
-}
-
 void print_arr() {
     for (int i = 0; i < n; i++) {
-        for (int j =0; j < n; j++)
-            cout << arr[i][j];
+        for (int j =0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main() {
-    int m, r;
+    int r;
     cin >> n >> m >> r;
     for (int i = 0; i < n; i++) {
         for (int j =0; j < n; j++)
             cin >> arr[i][j];
     } 
 
-    block_down(m, r - 1);
+    block_down(r - 1);
 
     print_arr();
     return 0;
