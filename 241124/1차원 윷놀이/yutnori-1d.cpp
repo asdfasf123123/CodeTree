b2c7ac824b9241b4c945261a26d1404957b1c882
asdfasf123123 <128668708+asdfasf123123@users.noreply.github.com>
2024-11-24 17:31:40 +0900

    Title: 1차원 윷놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/1차원 윷놀이/yutnori-1d.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int n, m, k;
+int mal[4];
+int dis[12];
+int maxNum = 0, sum = 0;
+
+void play(int cur_num) {
+    if (cur_num == n) {
+        sum = 0;
+        for (int i = 0; i< k; i++) {
+            if (mal[i] >= m)
+                sum++;
+        }
+        if (maxNum < sum) {
+            maxNum = sum;
+        }
+        return;
+    }
+
+    for (int i = 0; i < k; i++) {
+        if (mal[i] >= m) 
+            continue;
+        mal[i] += dis[cur_num];
+        play(cur_num + 1);
+        mal[i] += dis[cur_num];
+
+    }
+}
+
+int main() {
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> dis[i];
+    }
+    for (int i = 0; i < k; i++) {
+        mal[i] = 1;
+    }
+
+    play(0);
+
+    cout << maxNum;
+    return 0;
+}
